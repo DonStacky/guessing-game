86e9face61bb86e238c9b0c5bfb6679696b31576
Сергей (DonStacky) <rodnihsergey1994@mail.ru>
2023-02-24 14:44:06 +0300

    resolve

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.range = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.range = Math.round((this.max + this.min)/2);
+        return this.range;
     }
 
     lower() {
-
+        this.max = this.range;
+        console.log(`max = ${this.max}`);
+        this.guess();
     }
 
     greater() {
-
+        this.min = this.range;
+        console.log(`min = ${this.min}`);
+        this.guess();
     }
 }
 
